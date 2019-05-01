8fe5dfe82f4fdcd6ecba3b3331fde806276bfc47
autom8ter <admin@autom8ter.com>
2019-04-30 20:14:25 -0600

    update

--- fire.go
+++ fire.go
@@ -8,32 +8,31 @@ import (
 type Document interface {
 	DocCategory() string
 	DocName() string
-	DocData() map[string]interface{}
 }
 
 type Client struct {
 	Fire *firestore.Client
 }
 
-func NewClient(ctx context.Context, projectID string) (*Client,error) {
+func NewClient(ctx context.Context, projectID string) (*Client, error) {
 	client, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
 		return nil, err
 	}
 	return &Client{
 		Fire: client,
-		}, nil
+	}, nil
 }
 
-func (c *Client) Set(ctx context.Context, d Document, merge bool) error {
+func (c *Client) Set(ctx context.Context, d Document, data map[string]interface{}, merge bool) error {
 	if merge {
-		_, err :=c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Set(ctx, d.DocData(), firestore.MergeAll)
+		_, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Set(ctx, data, firestore.MergeAll)
 		if err != nil {
 			return err
 		}
 		return nil
 	} else {
-		_, err :=c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Set(ctx, d.DocData())
+		_, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Set(ctx, data)
 		if err != nil {
 			return err
 		}
@@ -42,24 +41,60 @@ func (c *Client) Set(ctx context.Context, d Document, merge bool) error {
 }
 
 func (c *Client) Snapshot(ctx context.Context, d Document) (*firestore.DocumentSnapshot, error) {
-	return  c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Get(ctx)
+	return c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Get(ctx)
 }
 
-func (c *Client) Ref(ctx context.Context, d Document) (*firestore.DocumentRef) {
-	return  c.Fire.Collection(d.DocCategory()).Doc(d.DocName())
+func (c *Client) Ref(ctx context.Context, d Document) *firestore.DocumentRef {
+	return c.Fire.Collection(d.DocCategory()).Doc(d.DocName())
 }
 
-func (c *Client) Create(ctx context.Context, d Document) (error) {
-	_, err :=  c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Create(ctx, d.DocData())
+func (c *Client) MarshalTo(ctx context.Context, d Document, obj interface{}) error {
+	snap, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Get(ctx)
+	if err != nil {
+		return err
+	}
+	return snap.DataTo(obj)
+}
+
+func (c *Client) DataAt(ctx context.Context, d Document, key string) (interface{}, error) {
+	snap, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return snap.DataAt(key)
+}
+
+func (c *Client) UpdateField(ctx context.Context, d Document, key string, value string) error {
+	_, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Update(ctx, []firestore.Update{
+		{
+			Path:  key,
+			Value: value,
+		},
+	})
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func (c *Client) Data(ctx context.Context, d Document) (map[string]interface{}, error) {
+	snap, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return snap.Data(), nil
+}
 
-func (c *Client) Delete(ctx context.Context, d Document) (error) {
-	_, err :=  c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Delete(ctx)
+func (c *Client) Create(ctx context.Context, d Document, data map[string]interface{}) error {
+	_, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Create(ctx, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) Delete(ctx context.Context, d Document) error {
+	_, err := c.Fire.Collection(d.DocCategory()).Doc(d.DocName()).Delete(ctx)
 	if err != nil {
 		return err
 	}
